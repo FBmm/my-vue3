cbc2528ef95cf7c9b31cfe5c7e2e9116196c3da6
wuqian <wuq_xd@163.com>
2022-04-01 18:00:52 +0800

    build 测试代码

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,4 +1,4 @@
-import { Test } from '../runtime-core'
+import { Test } from '../../runtime-core/src/index'
 
 const t = new Test('test')
 console.log(t.getName())
--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
 import ts from '@rollup/plugin-typescript';
+import path from 'path'
+
+const packagesDir = path.resolve(__dirname, 'packages/runtime-dom')
+
+const resolve = (p) => path.resolve(packagesDir, p)
+
 
 export default {
-  input: 'test/index.ts',
+  input: resolve('src/index.ts'),
   output: {
-    file: 'dist/dist.js',
-    format: 'cjs'
+    file: path.resolve(__dirname, `dist/main.esm-bundler.js`),
+    format: `es`
   },
   plugins: [
     ts()
